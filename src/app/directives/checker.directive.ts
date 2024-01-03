b66e9d790d9ad7b81fdd1fd5761ed11d5d1d4b4d
Taras Shevchuk <tarashevchuk0401@gmail.com>
2024-01-03 21:00:59 +0100

    styling added

--- src/app/directives/checker.directive.ts
+++ src/app/directives/checker.directive.ts
@@ -3,7 +3,6 @@ import {
   ElementRef,
   HostListener,
   Renderer2,
-  ViewChild,
 } from '@angular/core';
 
 @Directive({
@@ -14,48 +13,47 @@ export class CheckerDirective {
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   @HostListener('input', ['$event']) onInput(event: Event): void {
-    const inputValue: string = (event.target as HTMLInputElement).value;
+    const inputValue: string = (event.target as HTMLInputElement).value.trim();
 
     const hasLetters = /[a-zA-Zа-яА-ЯіІїЇєЄ]/.test(inputValue);
     const hasDigits = /\d/.test(inputValue);
     const hasSymbols = /[^a-zA-Zа-яА-ЯіІїЇєЄ0-9]/.test(inputValue);
 
     if (hasDigits || hasLetters || hasSymbols) {
-      this.setColor(0, 'red');
-      this.setColor(1, 'gray');
-      this.setColor(2, 'gray');
+      this.setStatusColor(0, 'red');
+      this.setStatusColor(1, 'gray');
+      this.setStatusColor(2, 'gray');
     }
 
     if (
       (hasLetters && hasDigits) ||
-      (hasLetters && hasSymbols) ||
       (hasDigits && hasSymbols) ||
       (hasLetters && hasSymbols)
     ) {
-      this.setColor(0, 'yellow');
-      this.setColor(1, 'yellow');
-      this.setColor(2, 'gray');
+      this.setStatusColor(0, 'yellow');
+      this.setStatusColor(1, 'yellow');
+      this.setStatusColor(2, 'gray');
     }
 
     if (hasLetters && hasDigits && hasSymbols) {
-      this.setColor(0, 'green');
-      this.setColor(1, 'green');
-      this.setColor(2, 'green');
+      this.setStatusColor(0, 'green');
+      this.setStatusColor(1, 'green');
+      this.setStatusColor(2, 'green');
     }
 
     if (inputValue.length < 8) {
-      this.setColor(0, 'red');
-      this.setColor(1, 'red');
-      this.setColor(2, 'red');
+      this.setStatusColor(0, 'red');
+      this.setStatusColor(1, 'red');
+      this.setStatusColor(2, 'red');
     }
     if (inputValue.length === 0) {
-      this.setColor(0, 'gray');
-      this.setColor(1, 'gray');
-      this.setColor(2, 'gray');
+      this.setStatusColor(0, 'gray');
+      this.setStatusColor(1, 'gray');
+      this.setStatusColor(2, 'gray');
     }
   }
 
-  setColor(number: number, color: string): void {
+  setStatusColor(number: number, color: string): void {
     const section_container: HTMLElement | null =
       this.el.nativeElement.parentElement.querySelector('.section_container');
 
--- src/app/password-checker/password-checker/password-checker.component.ts
+++ src/app/password-checker/password-checker/password-checker.component.ts
@@ -11,10 +11,5 @@ import { CommonModule } from '@angular/common';
   styleUrl: './password-checker.component.scss'
 })
 export class PasswordCheckerComponent {
-  // passwordInput: string = ''
-
-  // reset(){
-  //   this.passwordInput = ''
-  // }
-
+  
 }
